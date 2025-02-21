3f0ec28f0dbcda3b254149c6153a17f104ef46f4
HariHaran212 <hariharan2004121@gmail.com>
2025-02-21 13:35:57 +0530

    V-1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { use, useEffect, useState } from 'react'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import "../public/fonts/bootstrap-icons/font/bootstrap-icons.css";
-import '@fontsource/poppins';  // Defaults to 400 weight (regular)
-import '@fontsource/poppins/400.css'; // Import specific weight
-import '@fontsource/poppins/600.css'; // Import bold weight
+import '@fontsource/poppins';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/600.css';
 import './App.css'
 
 import Header from './Header'
@@ -25,23 +23,6 @@ function App() {
 
   return (
     <div className='App'>
-      {/* <nav>
-        <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>About</Link></li>
-        <li><Link to='/postpage'>Post Page</Link></li>
-        </ul>
-      </nav> */}
-      {/* <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/postpage' element={<PostLayout />}>
-          <Route index element={<PostPage />} />
-          <Route path=':id' element={<Post />} />
-          <Route path='newpost' element={<NewPost />} />
-        </Route>
-        <Route path='*' element={<Missing />} />
-      </Routes> */}
       <DataProvider>
         <Header title='Link Sphere'/>
         <Nav />
@@ -66,5 +47,3 @@ function App() {
 }
 
 export default App
-
-// npx json-server -p 3500 -w data/db.json i want to deploy a json file that returns a json formatted
--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -24,30 +24,7 @@ export const DataProvider = ({children}) => {
     useEffect( () => {
         setPosts(data);
     }, [data])
-    // useEffect( () => {
-    //   const fetchPosts = async () => {
-    //     try{
-    //       const response = await api.get('/posts')
-    //       setPosts(response.data)
-    //     }
-    //     catch(err) {
-    //       if(err.response) {
-    //         // Not in the 200 response range
-    //         console.log(err.response.data)
-    //         console.log(err.response.status)
-    //         console.log(err.response.headers)
-    //       }
-    //       else {
-    //         console.log(`Error: ${err.message}`)
-    //       }
-    //     }
-    //   }
-
-    //   fetchPosts()
-    // }, [])
-
-
-
+    
     useEffect( () => {
         const filteredResults = posts.filter(post =>
         ((post.body).toLowerCase()).includes(search.toLowerCase()) ||
